b2e249326403803cb4536eacdfda3a4bd494a2d9
katviofloneliness <80598507+katviofloneliness@users.noreply.github.com>
2021-06-01 21:34:23 +0200

    Create Project Euler #2: Even Fibonacci numbers.cpp

---
+++ Project Euler #2: Even Fibonacci numbers.cpp
@@ -0,0 +1,34 @@
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+long long int Fibonacci(long long int n)
+{    
+    long long int a = 0, b = 1,sum=0;
+     for(int i=0;;i++)
+     {
+        b += a; 
+        if (b > n){break;}
+        if (b%2==0){
+            sum+=b;      
+        }
+        a = b-a; 
+     }     
+     return sum;
+}
+
+int main() {
+    long cases;
+    long long int n;
+    cin >> cases;
+    while(cases--){
+        cin >> n;
+        cout <<Fibonacci(n) << endl;
+    } 
+    return 0;
+} 
