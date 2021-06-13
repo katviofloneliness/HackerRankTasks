03bf5107c22f08f573ec543460369df623aa96dd
katviofloneliness <80598507+katviofloneliness@users.noreply.github.com>
2021-06-13 07:06:12 +0200

    Update Finding Primes O(sqrt(n)).cpp

--- Finding Primes O(sqrt(n)).cpp
+++ Finding Primes O(sqrt(n)).cpp
@@ -1,20 +1,22 @@
-
+void FindPrimes(int &n) {
+    int sq = sqrt(n);
+    for (int i = 2; i <= sq; i++) {
+        if (n % i == 0 && n != 2) {
+            n = 1;
+            break;
+        }
+    }
+    if (n == 1) cout << "Not prime\n";
+    else cout << "Prime\n";
+}
 
 int main() {
-    int cases = 0;
-    long long n = 0, sq;
-    cin >> cases;
-    while (cases--) {
-        cin >> n;
-        sq = sqrt(n);
-        for (int i = 2; i <= sq; i++) {
-            if (n % i == 0 && n != 2) {
-                n=1;
-                break;
-            }
-        }
-        if (n == 1) cout << "Not prime\n";
-        else cout << "Prime\n";
-    }
-    return 0;
+	int cases = 0;
+	int n = 0;
+	cin >> cases;
+	while (cases--) {
+		cin >> n;
+        FindPrimes(n);
+	}
+	return 0;
 }
